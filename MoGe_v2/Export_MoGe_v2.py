a8904dc334862cf46ef74a18a01bede6ec5dbd50
DakeQQ <160212901+DakeQQ@users.noreply.github.com>
2025-11-27 18:22:24 +0800

    Update Export_MoGe_v2.py

--- MoGe_v2/Export_MoGe_v2.py
+++ MoGe_v2/Export_MoGe_v2.py
@@ -20,8 +20,8 @@ NUM_TOKENS = 3600                # Larger is finer but slower.
 FOCAL = None                     # Set None for auto else fixed. FOCAL here is the focal length relative to half the image diagonal.
 
 OUTPUT_BEV = True                # True for output the bird eye view occupancy map.
-SOBEL_KERNEL_SIZE = 3            # [3, 5] set for gradient map. Smaller is finer.
-DEFAULT_GRAD_THRESHOLD = 0.3     # Set a appropriate value for detected object. Smaller is finer.
+SOBEL_KERNEL_SIZE = 3            # [3, 5] set for gradient map.
+DEFAULT_GRAD_THRESHOLD = 0.3     # Set a appropriate value for detected object.
 BEV_WIDTH_METERS = 10.0
 BEV_DEPTH_METERS = 10.0
 
